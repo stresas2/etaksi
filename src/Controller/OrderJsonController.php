9b2b1c646a6c7f5615a096840ef376f4d9ba8256
Lukas Budrys <lukasbudrys01@gmail.com>
2020-05-24 17:36:33 +0300

    clear

--- src/Controller/OrderJsonController.php
+++ src/Controller/OrderJsonController.php
@@ -6,7 +6,6 @@ use App\Service\OrderService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\VarDumper\VarDumper;
 
 class OrderJsonController
 {
--- src/Form/CoffeeFormType.php
+++ src/Form/CoffeeFormType.php
@@ -6,7 +6,6 @@ use App\Entity\CoffeOrder;
 use App\Repository\CoffeOrderRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +15,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\VarDumper\VarDumper;
 
 class CoffeeFormType extends AbstractType
 {
